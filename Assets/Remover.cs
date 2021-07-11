ffaa2cc267bfecfa1f9bf5302fdf7240bdc8c62f
YerayBB <yeraybosoms@gmail.com>
2021-07-12 00:03:35 +0200

    last commit

---
+++ Assets/Remover.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class Remover : MonoBehaviour
+{
+    NetManagerV nm;
+    private bool justone;
+    // Start is called before the first frame update
+    void Start()
+    {
+        nm = GameObject.Find("NetworkManager").GetComponent<NetManagerV>();
+        if(gameObject.transform.parent.gameObject.GetComponent<PhotonView>().IsMine){
+            transform.GetChild(0).gameObject.SetActive(false);
+        }
+        justone = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!justone)
+        {
+            if (nm.neverever)
+            {
+                if (nm.avatarOn) gameObject.SetActive(false);
+                justone = true;
+            }
+        }
+    }
+}
